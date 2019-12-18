5ac0d4671f82920d1b941b31412e214295f5da6e
mglowinski <mck00@o2.pl>
2019-12-18 20:04:55 +0100

    add maping to school controller

--- src/main/java/com/mglowinski/school/controller/SchoolController.java
+++ src/main/java/com/mglowinski/school/controller/SchoolController.java
@@ -1,51 +1,64 @@
 package com.mglowinski.school.controller;
 
+import com.mglowinski.school.dto.SchoolDto;
 import com.mglowinski.school.model.School;
 import com.mglowinski.school.service.SchoolService;
+import com.mglowinski.school.utils.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/schools")
+@RequestMapping("/api")
 public class SchoolController {
 
     private final SchoolService schoolService;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public SchoolController(SchoolService schoolService) {
+    public SchoolController(SchoolService schoolService, ObjectMapper modelMapper) {
         this.schoolService = schoolService;
+        this.objectMapper = modelMapper;
     }
 
-    @GetMapping
-    public ResponseEntity<List<School>> getAllSchools() {
-        return ResponseEntity.ok(schoolService.getAllSchools());
+    @GetMapping("/schools")
+    public ResponseEntity<List<SchoolDto>> getAllSchools() {
+        List<School> schools = schoolService.getAllSchools();
+        List<SchoolDto> schoolsDto = schools.stream()
+                .map(objectMapper::mapSchoolEntityToDto)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(schoolsDto);
     }
 
-    @PostMapping
-    public ResponseEntity<School> createSchool(@Valid @RequestBody School school) {
-        return ResponseEntity.status(201).body(schoolService.createSchool(school));
+    @PostMapping("/schools")
+    public ResponseEntity<SchoolDto> createSchool(@Valid @RequestBody SchoolDto schoolDto) {
+        School school = objectMapper.mapSchoolDtoToEntity(schoolDto);
+        School schoolCreated = schoolService.createSchool(school);
+        return ResponseEntity.status(HttpStatus.CREATED).body(objectMapper.mapSchoolEntityToDto(schoolCreated));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<School> getSchoolById(@PathVariable Long id) {
-        return ResponseEntity.status(200).body(schoolService.getSchoolById(id));
+    @GetMapping("/schools/{id}")
+    public ResponseEntity<SchoolDto> getSchoolById(@PathVariable Long id) {
+        SchoolDto schoolDto = objectMapper.mapSchoolEntityToDto(schoolService.getSchoolById(id));
+        return ResponseEntity.status(HttpStatus.OK).body(schoolDto);
     }
 
-    @PatchMapping("/{id}/address")
-    public ResponseEntity<Void> updateSchoolAddress(@PathVariable Long id,
-                                                    @RequestBody Map<String, Object> updateFields) {
-        schoolService.updateAddress(id, updateFields);
+    @DeleteMapping("/schools/{id}")
+    public ResponseEntity<Void> deleteSchoolById(@PathVariable Long id) {
+        schoolService.deleteSchoolById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSchoolById(@PathVariable Long id) {
-        schoolService.deleteSchoolById(id);
+    @PatchMapping("/schools/{id}/address")
+    public ResponseEntity<Void> updateSchoolAddress(@PathVariable Long id,
+                                                    @RequestBody Map<String, Object> updateFields) {
+        schoolService.updateAddress(id, updateFields);
         return ResponseEntity.noContent().build();
     }
 
