22f687d10ef73fbd0daf1f4723e78f2aebae4a6d
mglowinski <mck00@o2.pl>
2019-12-19 23:21:20 +0100

    add repository method to find all subjects by school id

--- src/main/java/com/mglowinski/school/repository/SubjectRepository.java
+++ src/main/java/com/mglowinski/school/repository/SubjectRepository.java
@@ -4,7 +4,11 @@ import com.mglowinski.school.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+
+    List<Subject> findAllBySchoolId(Long schoolId);
 
 }
