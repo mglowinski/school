cab3273958be527f3458b80054397247f3eae7d3
mglowinski <mck00@o2.pl>
2019-12-26 23:19:08 +0100

    add teacher model and dto

---
+++ src/main/java/com/mglowinski/school/dto/TeacherDto.java
@@ -0,0 +1,30 @@
+package com.mglowinski.school.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeacherDto implements Serializable {
+
+    private static final long serialVersionUID = 3662233273296427789L;
+
+    private Long id;
+
+    @NotNull(message = "Teacher firstName must be provided")
+    private String firstName;
+
+    @NotNull(message = "Teacher lastName must be provided")
+    private String lastName;
+
+    @NotNull(message = "Teacher phoneNumber must be provided")
+    private String phoneNumber;
+
+    @NotNull(message = "Teacher email must be provided")
+    private String email;
+}
---
+++ src/main/java/com/mglowinski/school/model/Teacher.java
@@ -0,0 +1,30 @@
+package com.mglowinski.school.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private School school;
+}
