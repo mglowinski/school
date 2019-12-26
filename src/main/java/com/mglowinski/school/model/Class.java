dac3eb56025bd816c90b3de21829f91479cbd3a5
mglowinski <mck00@o2.pl>
2019-12-26 20:49:55 +0100

    add class model

---
+++ src/main/java/com/mglowinski/school/model/Class.java
@@ -0,0 +1,28 @@
+package com.mglowinski.school.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Class {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String level;
+
+    private String name;
+
+    private String specialization;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private School school;
+}
