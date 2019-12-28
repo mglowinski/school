2ce10c8caff3138850cf7ec794b15eb866fcf1bb
mglowinski <mck00@o2.pl>
2019-12-28 22:31:58 +0100

    add create teacher dto

---
+++ src/main/java/com/mglowinski/school/dto/CreateTeacherDto.java
@@ -0,0 +1,30 @@
+package com.mglowinski.school.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateTeacherDto implements Serializable {
+
+    private static final long serialVersionUID = -3417056965669018976L;
+
+    private Long id;
+
+    @NotNull(message = "Teacher firstName must be provided")
+    private String firstName;
+
+    @NotNull(message = "Teacher lastName must be provided")
+    private String lastName;
+
+    @NotNull(message = "Teacher phoneNumber must be provided")
+    private String phoneNumber;
+
+    @NotNull(message = "Teacher email must be provided")
+    private String email;
+}
