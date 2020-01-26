7d09fd3be1d00872ba6899b048faaae4e229ba32
mglowinski <mck00@o2.pl>
2020-01-26 19:02:41 +0100

    add issued grade controller

---
+++ src/main/java/com/mglowinski/school/controller/IssuedGradeController.java
@@ -0,0 +1,47 @@
+package com.mglowinski.school.controller;
+
+import com.mglowinski.school.dto.CreateIssuedGradeDto;
+import com.mglowinski.school.dto.IssuedGradeDto;
+import com.mglowinski.school.model.IssuedGrade;
+import com.mglowinski.school.service.IssuedGradeService;
+import com.mglowinski.school.utils.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class IssuedGradeController {
+
+    private final IssuedGradeService issuedGradeService;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public IssuedGradeController(IssuedGradeService issuedGradeService,
+                                 ObjectMapper objectMapper) {
+        this.issuedGradeService = issuedGradeService;
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping("/classes/{classId}/issued-grades")
+    public ResponseEntity<List<IssuedGradeDto>> getIssuedGradesByClassId(@PathVariable Long classId) {
+        List<IssuedGrade> issuedGrades = issuedGradeService.getIssuedGradesByClassId(classId);
+        List<IssuedGradeDto> issuedGradesDto = issuedGrades.stream()
+                .map(objectMapper::mapIssuedGradeEntityToIssuedGradeDto)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(issuedGradesDto);
+    }
+
+    @PostMapping("/schools/{schoolId}/classes/{classId}/issued-grades")
+    public ResponseEntity<Void> createIssuedGrade(@PathVariable Long schoolId,
+                                                  @PathVariable Long classId,
+                                                  @Valid @RequestBody CreateIssuedGradeDto createIssuedGradeDto) {
+        issuedGradeService.createIssuedGrade(schoolId, classId, createIssuedGradeDto);
+        return ResponseEntity.noContent().build();
+    }
+
+}
