0fcacbf650dbe67c2dd0c16f7c731e9ff0110ab0
mglowinski <mck00@o2.pl>
2020-01-26 18:59:51 +0100

    update issued grade model

--- src/main/java/com/mglowinski/school/model/IssuedGrade.java
+++ src/main/java/com/mglowinski/school/model/IssuedGrade.java
@@ -1,22 +1,31 @@
 package com.mglowinski.school.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
+@Builder
+@Table(name = "Issued_Grade")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Diary {
+public class IssuedGrade {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    @ManyToOne
     private Class schoolClass;
 
     @ManyToOne
