4bcf7d12145a5f49cdf955118fbb3a7ae1d84936
mglowinski <mck00@o2.pl>
2020-01-31 20:38:26 +0100

    find subject-teacher pair by subject id and teacher id

--- src/main/java/com/mglowinski/school/repository/SubjectTeacherRepository.java
+++ src/main/java/com/mglowinski/school/repository/SubjectTeacherRepository.java
@@ -4,7 +4,10 @@ import com.mglowinski.school.model.SubjectTeacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SubjectTeacherRepository extends JpaRepository<SubjectTeacher, Long> {
 
+    Optional<SubjectTeacher> findBySubjectIdAndTeacherId(Long subjectId, Long teacherId);
 }
